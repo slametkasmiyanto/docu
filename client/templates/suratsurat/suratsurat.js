508dc9fb34bd9b41af3904915a3e0943ce94d09a
slametkasmiyanto <slamet.kasmiyanto@gmail.com>
2017-08-14 12:19:28 +0700

    Entri berkas

--- client/templates/suratsurat/suratsurat.js
+++ client/templates/suratsurat/suratsurat.js
@@ -1,13 +1,13 @@
 Template.suratsurat.events({
-    'submit .unggahSuratForm': function(event){
-        var tanggal = event.target.tanggal.value;
-        var nomor = event.target.nomor.value;
-        var perihal = event.target.perihal.value;
-        var kategori = event.target.kategori.value;
-        var sifat = event.target.sifat.value;
-        var tujuan = event.target.tujuan.value;
-        var pesan = event.target.pesan.value;
-    
+    "submit .unggahSuratForm": function(event){
+        var tanggal = trimInput(event.target.tanggal.value);
+        var nomor = trimInput(event.target.nomor.value);
+        var perihal = trimInput(event.target.perihal.value);
+        var kategori = trimInput(event.target.kategori.value);
+        var sifat = trimInput(event.target.sifat.value);
+        var tujuan = trimInput(event.target.tujuan.value);
+        var pesan = trimInput(event.target.pesan.value);
+        var status = "New";
 
         SuratSurat.insert({
             tanggal: tanggal,
@@ -19,10 +19,15 @@ Template.suratsurat.events({
             pesan: pesan,
             pengirim: Meteor.userId(),
             createdAt: new Date(),
+            status: status,
+        });
 
-        })
-
-        FlashMessages.sendError("Surat berhasil diunggah.");
+        $('#suratsuratModal').modal('hide');
+        FlashMessages.sendSuccess("Surat berhasil diunggah.");
         return false;
     }
 });
+
+var trimInput = function(val){
+    return val.replace(/^\s*|\s*$/g,"");
+}
